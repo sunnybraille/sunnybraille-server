b67739d82131342fa0332ac206d21976ebec9d30
깃짱 <107979804+gitchannn@users.noreply.github.com>
2024-02-04 23:37:27 +0900

    feat: initial commit (#2)

---
+++ server/src/test/java/sunflower/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package sunflower.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
