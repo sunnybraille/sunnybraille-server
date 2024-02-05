ae30fceecf3d90f5f39764ad456497a02e1e2653
gitchan <eunkeeeee@gmail.com>
2024-02-05 20:22:54 +0900

    test: health check api 테스트 코드 작성

---
+++ server/src/test/java/sunflower/server/acceptance/HealthCheckAcceptanceTest.java
@@ -0,0 +1,29 @@
+package sunflower.server.acceptance;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+
+import static io.restassured.http.ContentType.JSON;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HealthCheckAcceptanceTest {
+
+    @Test
+    void 서버_헬스_체크() {
+        final ExtractableResponse<Response> response = RestAssured.given()
+                .log().all()
+                .contentType(JSON)
+
+                .when()
+                .get("/health")
+
+                .then()
+                .log().all()
+                .extract();
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+    }
+}
