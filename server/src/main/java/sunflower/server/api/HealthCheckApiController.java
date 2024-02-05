b827186e8b118c68332369edaf2946cfa1cf56f5
gitchan <eunkeeeee@gmail.com>
2024-02-05 20:18:14 +0900

    feat: health check api 생성

---
+++ server/src/main/java/sunflower/server/api/HealthCheckApiController.java
@@ -0,0 +1,16 @@
+package sunflower.server.api;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+@Controller
+public class HealthCheckApiController {
+
+    @RequestMapping(method = GET, path = "/health")
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("OK");
+    }
+}
