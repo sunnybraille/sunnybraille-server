38d4f856ce6bc1ac7a65834ec11d6c4d73a475d6
gitchan <eunkeeeee@gmail.com>
2024-02-08 10:03:16 +0900

    feat: autowire app-id, app-key

--- server/src/main/java/sunflower/server/application/PdfTranslationService.java
+++ server/src/main/java/sunflower/server/application/PdfTranslationService.java
@@ -1,10 +1,22 @@
 package sunflower.server.application;
 
+import lombok.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class PdfTranslationService {
+
+    private final String appId;
+    private final String appKey;
+
+    public PdfTranslationService(
+            @Value("{mathpix.app-id}") String appId,
+            @Value("{mathpix.app-key}") String appKey
+    ) {
+        this.appId = appId;
+        this.appKey = appKey;
+    }
 
     public Long translate(final MultipartFile file) {
         return null;
