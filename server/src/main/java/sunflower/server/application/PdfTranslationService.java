b19122d312a348b649f62b31eba2986264c95bf9
gitchan <eunkeeeee@gmail.com>
2024-02-08 10:11:57 +0900

    fix: submodules로부터 변수 가져오도록 변경

--- server/src/main/java/sunflower/server/application/PdfTranslationService.java
+++ server/src/main/java/sunflower/server/application/PdfTranslationService.java
@@ -1,18 +1,20 @@
 package sunflower.server.application;
 
-import lombok.Value;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
+@Profile("!test")
 public class PdfTranslationService {
 
     private final String appId;
     private final String appKey;
 
     public PdfTranslationService(
-            @Value("{mathpix.app-id}") String appId,
-            @Value("{mathpix.app-key}") String appKey
+            @Value("${mathpix.app-id}") String appId,
+            @Value("${mathpix.app-key}") String appKey
     ) {
         this.appId = appId;
         this.appKey = appKey;
