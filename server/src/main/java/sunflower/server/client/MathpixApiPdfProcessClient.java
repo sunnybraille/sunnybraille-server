87e994c6fe1f7414ec064d4d424f4428b9e48091
gitchan <eunkeeeee@gmail.com>
2024-02-14 15:55:45 +0900

    refactor: api uri는 secret에서 제거

--- server/src/main/java/sunflower/server/client/MathpixApiPdfProcessClient.java
+++ server/src/main/java/sunflower/server/client/MathpixApiPdfProcessClient.java
@@ -25,18 +25,16 @@ import static org.springframework.http.MediaType.MULTIPART_FORM_DATA;
 @Component
 public class MathpixApiPdfProcessClient {
 
-    private final String appURI;
+    private static final String APP_URI = "https://api.mathpix.com/v3/pdf";
     private final String appId;
     private final String appKey;
     private final RestTemplate restTemplate;
 
     public MathpixApiPdfProcessClient(
-            @Value("${mathpix.app-uri}") String appURI,
             @Value("${mathpix.app-id}") String appId,
             @Value("${mathpix.app-key}") String appKey,
             RestTemplate restTemplate
     ) {
-        this.appURI = appURI;
         this.appId = appId;
         this.appKey = appKey;
         this.restTemplate = restTemplate;
@@ -49,12 +47,12 @@ public class MathpixApiPdfProcessClient {
         final MultiValueMap<String, Object> requestBody = createRequestBody(file, objectMapper);
         final HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(requestBody, requestHeader);
 
-        log.info("Request URI: {}", appURI);
+        log.info("Request URI: {}", APP_URI);
         log.info("Request Headers: {}", requestHeader);
         log.info("Request Parameters: {}", requestBody);
 
         // send request to Mathpix API (process a pdf)
-        final ResponseEntity<String> response = restTemplate.postForEntity(appURI, requestEntity, String.class);
+        final ResponseEntity<String> response = restTemplate.postForEntity(APP_URI, requestEntity, String.class);
 
         if (response.getStatusCode() != HttpStatus.OK) {
             log.warn("OCR 작업에 실패했습니다. Mathpix API 에러 메세지: {}", response.getBody());
