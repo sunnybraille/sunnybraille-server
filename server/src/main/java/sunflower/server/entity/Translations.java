06618563351a4c7d47c1e259456bfad4ac0ca14b
깃짱 <107979804+gitchannn@users.noreply.github.com>
2024-02-21 17:29:11 +0900

    feat: 점역 진행 상황 확인 API 개발 (#32)
    * chore: gitignore 추가
    
    * feat: 예외 처리
    
    * feat: 점역 status 반환 API 생성

--- server/src/main/java/sunflower/server/entity/Translations.java
+++ server/src/main/java/sunflower/server/entity/Translations.java
@@ -1,16 +1,22 @@
 package sunflower.server.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import sunflower.server.client.dto.OcrProgressStatus;
 
+import static jakarta.persistence.EnumType.STRING;
 import static jakarta.persistence.GenerationType.IDENTITY;
 import static lombok.AccessLevel.PROTECTED;
 
 @Getter
+@Setter
 @NoArgsConstructor(access = PROTECTED)
 @AllArgsConstructor
 @Entity
@@ -19,17 +25,21 @@ public class Translations {
     @Id
     @GeneratedValue(strategy = IDENTITY)
     private Long id;
+
     private String pdfURI;
     private String ocrPdfId;
-    private Double ocrPercentDone;
+
+    @Enumerated(STRING)
+    private OcrProgressStatus ocrProgressStatus;
+
+    private Integer ocrPercentDone;
     private String ocrLatexFileURI;
     private String brfFileURI;
-
-    public Translations(final String pdfURI) {
-        this.pdfURI = pdfURI;
-    }
+    private Integer brailleTranslationPercentDone;
 
     public static Translations of(final String pdfURI) {
-        return new Translations(null, pdfURI, null, null, null, null);
+        final Translations translations = new Translations();
+        translations.setPdfURI(pdfURI);
+        return translations;
     }
 }
