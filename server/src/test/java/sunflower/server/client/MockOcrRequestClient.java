b56013ab716cc0e73950f4269f92f9302e7c31b9
gitchan <eunkeeeee@gmail.com>
2024-02-22 01:13:25 +0900

    test: mock 객체 로직 변경

--- server/src/test/java/sunflower/server/client/MockOcrRequestClient.java
+++ server/src/test/java/sunflower/server/client/MockOcrRequestClient.java
@@ -1,29 +1,21 @@
 package sunflower.server.client;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.Date;
+import java.io.File;
 import java.util.UUID;
 
 @Component
 public class MockOcrRequestClient implements OcrRequestClient {
 
-    private static final String SECRET_KEY = "haebaragi-secret";
-
     @Override
     public String requestPdfId(final MultipartFile file) {
-        // create random pdf id
-        long expirationMs = 3600000;
-        Date expiryDate = new Date(System.currentTimeMillis() + expirationMs);
+        return UUID.randomUUID().toString()
+    }
 
-        return Jwts.builder()
-                .setId(UUID.randomUUID().toString())
-                .setIssuedAt(new Date())
-                .setExpiration(expiryDate)
-                .signWith(SignatureAlgorithm.HS512, SECRET_KEY)
-                .compact();
+    @Override
+    public String requestPdfId(final File file) {
+        return UUID.randomUUID().toString();
     }
 }
