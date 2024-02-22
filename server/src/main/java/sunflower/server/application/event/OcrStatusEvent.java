d6f9ee046474c9f281bff341c9e0c5549dde9586
gitchan <eunkeeeee@gmail.com>
2024-02-22 15:31:52 +0900

    feat: OcrStatusEvent 정의

--- server/src/main/java/sunflower/server/application/event/OcrStatusEvent.java
+++ server/src/main/java/sunflower/server/application/event/OcrStatusEvent.java
@@ -6,10 +6,12 @@ import org.springframework.context.ApplicationEvent;
 @Getter
 public class OcrStatusEvent extends ApplicationEvent {
 
+    private final Long id;
     private final String pdfId;
 
-    public OcrStatusEvent(final Object source, final String pdfId) {
+    public OcrStatusEvent(final Object source, final Long id, final String pdfId) {
         super(source);
+        this.id = id;
         this.pdfId = pdfId;
     }
 }
