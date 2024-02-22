c4935d00bee1728074445d056dd2457bd0b5a924
gitchan <eunkeeeee@gmail.com>
2024-02-22 14:40:44 +0900

    refactor: 불필요한 코드 제거

--- server/src/main/java/sunflower/server/application/TranslationsSaveEventListener.java
+++ server/src/main/java/sunflower/server/application/TranslationsSaveEventListener.java
@@ -5,20 +5,20 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 import sunflower.server.application.event.TranslationsSaveEvent;
 import sunflower.server.client.OcrDownloadClient;
-import sunflower.server.client.OcrStatusClient;
 import sunflower.server.client.OcrRegisterClient;
+import sunflower.server.client.OcrStatusClient;
 import sunflower.server.entity.Translations;
 import sunflower.server.repository.TranslationsRepository;
 
 import java.io.File;
 import java.nio.file.Paths;
 import java.util.NoSuchElementException;
+
+import static org.springframework.transaction.annotation.Propagation.REQUIRES_NEW;
 
 @Slf4j
 @NoArgsConstructor
@@ -44,19 +44,18 @@ public class TranslationsSaveEventListener {
     }
 
     @Async
-    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    @TransactionalEventListener
+    @Transactional(propagation = REQUIRES_NEW)
     public void handleTranslationsSave(final TranslationsSaveEvent event) {
         log.info("현재 스레드: {}", Thread.currentThread().getName());
 
         final Translations translations = translationsRepository.findById(event.getTranslations().getId())
                 .orElseThrow(() -> new NoSuchElementException("존재하지 않는 Translation입니다!"));
-        final String pdfURI = translations.getPdfURI().replace("file:", ""); // 일단 어쩔 수 없이 했는데 코드 파악 필요
+        final String pdfURI = translations.getPdfURI().replace("file:", ""); // 확인 필요
         final File file = Paths.get(pdfURI).toFile();
 
         translations.startOcr();
         final String pdfId = ocrRegisterClient.requestPdfId(file);
         translations.setOcrPdfId(pdfId);
-        translationsRepository.save(translations);
     }
 }
