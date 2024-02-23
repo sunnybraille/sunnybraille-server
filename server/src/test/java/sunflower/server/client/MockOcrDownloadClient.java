f52ff502ab6ed8d48ce1b9b04d46a6164903d710
gitchan <eunkeeeee@gmail.com>
2024-02-24 01:57:40 +0900

    fix: 컴파일 에러 해결

--- server/src/test/java/sunflower/server/client/MockOcrDownloadClient.java
+++ server/src/test/java/sunflower/server/client/MockOcrDownloadClient.java
@@ -3,14 +3,12 @@ package sunflower.server.client;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-import java.io.File;
-
 @Component
 @Profile("test")
 public class MockOcrDownloadClient implements OcrDownloadClient {
 
     @Override
     public byte[] download(final String pdfId) {
-        return new File("");
+        return null;
     }
 }
