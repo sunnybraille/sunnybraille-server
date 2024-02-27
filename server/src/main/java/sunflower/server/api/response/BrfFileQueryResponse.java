20b618a035021cf500c8306657513cb91655a322
gitchan <eunkeeeee@gmail.com>
2024-02-28 01:28:23 +0900

    feat: Swagger 작성

--- server/src/main/java/sunflower/server/api/response/BrfFileQueryResponse.java
+++ server/src/main/java/sunflower/server/api/response/BrfFileQueryResponse.java
@@ -1,11 +1,16 @@
 package sunflower.server.api.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 @Getter
 public class BrfFileQueryResponse {
 
+    @Schema(description = "Translations ID", defaultValue = "1")
     private final Long id;
+
+    @Schema(description = "brf 파일 내용",
+            defaultValue = "  \\documentclass82#ajpt;081article\"0\n\\usepackage82utf#h;081inputenc\"0\n\\usepackage82T#a;081fontenc\"0\n\\usepackage81CJKutf#h\"0\n\\usepackage81amsmath\"0\n\\usepackage81amsfonts\"0\n\\usepackage81amssymb\"0\n\\usepackage82version=#d;081mhchem\"0\n\\usepackage81stmaryrd\"0\n\n\\begin81document\"0\n\\begin81CJK\"081UTF#h\"081mj\"0\n\\begin81enumerate\"0\n  \\setcounter81enumi\"081#bg\"0\n  \\item $p<q$ q im ,u,m $p\" q$ n irj<: $p^81#b\"0 q<n \\leq p q^81#b\"0$ ! e3.x,ofocz .<*,m $n$ w @r,m$ #cjh o1 ,ir\" $p+q$ w $b'! @mj,ou4 82#d.s5;0\n\\end81enumerate\"0\n\n\\end81CJK\"0\n\\end81document\"0â ")
     private final String content;
 
     public BrfFileQueryResponse(final Long id, final String content) {
--- server/src/main/java/sunflower/server/api/response/TranslationStatusResponse.java
+++ server/src/main/java/sunflower/server/api/response/TranslationStatusResponse.java
@@ -1,5 +1,6 @@
 package sunflower.server.api.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import sunflower.server.application.dto.TranslationStatusDto;
@@ -8,10 +9,21 @@ import sunflower.server.application.dto.TranslationStatusDto;
 @RequiredArgsConstructor
 public class TranslationStatusResponse {
 
+    @Schema(description = "Translations ID", defaultValue = "1")
     private final Long id;
+
+    @Schema(description = "OCR 진행 상태", defaultValue = "PROCESSING",
+            allowableValues = {"NONE", "PROCESSING", "COMPLETED"})
     private final String ocrStatus;
+
+    @Schema(description = "OCR 진행도", defaultValue = "30%")
     private final Integer ocrPercentDone;
+
+    @Schema(description = "점역 진행 상태", defaultValue = "NONE",
+            allowableValues = {"NONE", "PROCESSING", "COMPLETED"})
     private final String brailleTranslationsStatus;
+
+    @Schema(description = "점역 진행도", defaultValue = "0%")
     private final Integer brailleTranslationPercentDone;
 
     public static TranslationStatusResponse from(final TranslationStatusDto dto) {
