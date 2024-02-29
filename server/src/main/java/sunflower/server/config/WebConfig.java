a660773ab94352930cb12f41e36c105a905a322b
깃짱 <107979804+gitchannn@users.noreply.github.com>
2024-02-29 14:43:51 +0900

    fix: localhost 3000 port에 대해 cors 설정 (#71)

--- server/src/main/java/sunflower/server/config/WebConfig.java
+++ server/src/main/java/sunflower/server/config/WebConfig.java
@@ -1,27 +1,29 @@
 package sunflower.server.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-//    private static final String[] ALLOWED_ORIGINS = {"**"};
-//
-//    @Override
-//    public void addCorsMappings(final CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOrigins(ALLOWED_ORIGINS)
-//                .allowedMethods(
-//                        HttpMethod.GET.name(),
-//                        HttpMethod.POST.name(),
-//                        HttpMethod.PUT.name(),
-//                        HttpMethod.PATCH.name(),
-//                        HttpMethod.DELETE.name(),
-//                        HttpMethod.OPTIONS.name()
-//                )
-//                .allowedHeaders("*")
-//                .exposedHeaders("Location")
-//                .allowCredentials(true);
-//    }
+    private static final String[] ALLOWED_ORIGINS = {"**"};
+
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.PATCH.name(),
+                        HttpMethod.DELETE.name(),
+                        HttpMethod.OPTIONS.name()
+                )
+                .allowedHeaders("*")
+                .exposedHeaders("Location")
+                .allowCredentials(true);
+    }
 }
