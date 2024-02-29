ce8f3a70f0560648e3e2aac9ef6032fd5586a56d
깃짱 <107979804+gitchannn@users.noreply.github.com>
2024-02-29 17:36:38 +0900

    feat: client ip cors 설정 추가 (#74)
    * feat: client ip cors 설정 추가
    
    * feat: update submodules
    
    * feat: test용 ip 추가

--- server/src/main/java/sunflower/server/config/WebConfig.java
+++ server/src/main/java/sunflower/server/config/WebConfig.java
@@ -1,5 +1,6 @@
 package sunflower.server.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,12 +9,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    private static final String[] ALLOWED_ORIGINS = {"**"};
+    private final String appURI;
+
+    public WebConfig(@Value("${client.uri}") String appURI) {
+        this.appURI = appURI;
+    }
 
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000", appURI)
                 .allowedMethods(
                         HttpMethod.GET.name(),
                         HttpMethod.POST.name(),
