bbe29c47c6aaf67e2ba113b9abb195a12945ecb2
깃짱 <107979804+gitchannn@users.noreply.github.com>
2024-03-11 00:06:01 +0900

    docs: 인증 관련 API 문서화 (#124)
    * docs: 로그인/회원가입 API 문서화
    
    * docs: 아이디, 비밀번호 swagger 추가

---
+++ server/src/main/java/sunflower/server/api/MemberApiControllerDocs.java
@@ -0,0 +1,26 @@
+package sunflower.server.api;
+
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import sunflower.server.api.request.JoinRequest;
+import sunflower.server.api.request.LoginRequest;
+
+@Tag(name = "회원가입/로그인 API", description = "Sunny Braille 회원 관련 API로, 점역 기능 사용을 위해 필요합니다.")
+public interface MemberApiControllerDocs {
+
+    @Operation(summary = "회원가입 API", description = "요청한 아이디, 비밀번호는 데이터베이스에 저장됩니다. (비밀번호는 암호화됩니다)")
+    @ApiResponse(responseCode = "200", description = "회원가입이 완료되었습니다. 다시  로그인해 주세요")
+    ResponseEntity<Void> join(@RequestBody JoinRequest request);
+
+    @Operation(summary = "로그인 API", description = "회원가입 시 입력했던 아이디, 비밀번호를 전송해 주세요.")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "sessionId", value = "세션 식별자", required = true, dataType = "string", paramType = "cookie")
+    })
+    ResponseEntity<Void> login(@RequestBody LoginRequest request, HttpServletResponse response);
+}
--- server/src/main/java/sunflower/server/api/request/JoinRequest.java
+++ server/src/main/java/sunflower/server/api/request/JoinRequest.java
@@ -1,12 +1,18 @@
 package sunflower.server.api.request;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Getter
+@ApiModel(description = "회원 가입 요청")
 public class JoinRequest {
 
+    @ApiModelProperty(value = "로그인 아이디", example = "gitchan", required = true)
     private final String loginId;
+
+    @ApiModelProperty(value = "비밀번호", example = "gitchan123", required = true)
     private final String password;
 }
