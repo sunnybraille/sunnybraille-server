bdea189b706491cf698c024c1c6f5428b7ecf32f
gitchan <eunkeeeee@gmail.com>
2024-03-10 16:42:54 +0900

    refactor: jwt token 관련 클래스 삭제

--- server/src/main/java/sunflower/server/api/MemberApiController.java
+++ server/src/main/java/sunflower/server/api/MemberApiController.java
@@ -8,16 +8,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import sunflower.server.api.request.JoinRequest;
 import sunflower.server.api.request.LoginRequest;
-import sunflower.server.api.response.AuthTokensResponse;
 import sunflower.server.application.MemberService;
-import sunflower.server.util.AuthTokensGenerator;
 
 @RequiredArgsConstructor
 @RestController
 public class MemberApiController {
 
     private final MemberService memberService;
-    private final AuthTokensGenerator authTokensGenerator;
 
     @PostMapping("/join")
     ResponseEntity<Void> join(@RequestBody JoinRequest request) {
@@ -26,8 +23,8 @@ public class MemberApiController {
     }
 
     @PostMapping("/login")
-    ResponseEntity<AuthTokensResponse> login(@RequestBody LoginRequest request) {
+    ResponseEntity<Void> login(@RequestBody LoginRequest request) {
         final Long id = memberService.login(request.getLoginId(), request.getPassword());
-        return ResponseEntity.ok(authTokensGenerator.generateToken(id));
+        return null;
     }
 }
