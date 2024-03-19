79b750b52a9a45afa302b5a0fe2ebf54e03e8bfc
gitchan <eunkeeeee@gmail.com>
2024-03-19 15:04:34 +0900

    feat: user info를 가져오는 client 추가

--- server/src/main/java/sunflower/server/auth/application/KakaoMemberService.java
+++ server/src/main/java/sunflower/server/auth/application/KakaoMemberService.java
@@ -2,7 +2,9 @@ package sunflower.server.auth.application;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import sunflower.server.auth.client.KakaoOAuthClient;
+import sunflower.server.auth.client.KakaoAccessTokenClient;
+import sunflower.server.auth.client.KakaoUserProfileClient;
+import sunflower.server.auth.client.response.KakaoOAuthResponse;
 
 @Service
 public class KakaoMemberService {
@@ -11,20 +13,26 @@ public class KakaoMemberService {
     private final String restApiKey;
     private final String authCodeURI;
     private final String authTokenURI;
-    private final KakaoOAuthClient kakaoOAuthClient;
+    private final String userProfileURI;
+    private final KakaoAccessTokenClient kakaoAccessTokenClient;
+    private final KakaoUserProfileClient kakaoUserProfileClient;
 
     public KakaoMemberService(
             @Value("${oauth.kakao.rest-api-key}") String restApiKey,
             @Value("${oauth.kakao.redirect-uri}") String redirectURI,
             @Value("${oauth.kakao.auth-code-uri}") String authCodeURI,
             @Value("${oauth.kakao.token-uri}") String authTokenURI,
-            final KakaoOAuthClient kakaoOAuthClient
+            @Value("${oauth.kakao.user-info-request-uri}") String userProfileURI,
+            final KakaoAccessTokenClient kakaoAccessTokenClient,
+            final KakaoUserProfileClient kakaoUserProfileClient
     ) {
         this.redirectURI = redirectURI;
         this.restApiKey = restApiKey;
         this.authCodeURI = authCodeURI;
         this.authTokenURI = authTokenURI;
-        this.kakaoOAuthClient = kakaoOAuthClient;
+        this.userProfileURI = userProfileURI;
+        this.kakaoAccessTokenClient = kakaoAccessTokenClient;
+        this.kakaoUserProfileClient = kakaoUserProfileClient;
     }
 
     public String loginURI() {
@@ -32,7 +40,10 @@ public class KakaoMemberService {
     }
 
     public String login(final String code) {
-        kakaoOAuthClient.login(authTokenURI, restApiKey, redirectURI, code);
+        final KakaoOAuthResponse response = kakaoAccessTokenClient.requestAccessToken(authTokenURI, restApiKey, redirectURI, code);
+        final String accessToken = response.getAccessToken();
+        kakaoUserProfileClient.requestUserProfile(userProfileURI, accessToken);
+
         return null;
     }
 }
